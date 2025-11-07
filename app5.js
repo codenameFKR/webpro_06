565361431bca01b2c9ec951f5d7078bbdf79a312
Runa Kawase <haruachan0321@gmail.com>
2025-11-07 12:31:19 +0900

    ‘/keiyo_addを追加したよ’

--- app5.js
+++ app5.js
@@ -18,11 +18,19 @@ app.get("/icon", (req, res) => {
   res.render('icon', { filename:"./public/Apple_logo_black.svg", alt:"Apple Logo"});
 });
 
+app.get("/omikuji", (req, res) => {
+  res.render('omikuji');
+});
 app.get("/omikuji1", (req, res) => {
   const num = Math.floor( Math.random() * 6 + 1 );
   let luck = '';
-  if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
+  if (num == 1) luck = '大吉';
+  else if (num == 2) luck = '吉';
+  else if (num == 3) luck = '中吉';
+  else if (num == 4) luck = '小吉';
+  else if (num == 5) luck = '末吉';
+  else if (num == 6) luck = '凶';
+  
 
   res.send( '今日の運勢は' + luck + 'です' );
 });
@@ -31,11 +39,93 @@ app.get("/omikuji2", (req, res) => {
   const num = Math.floor( Math.random() * 6 + 1 );
   let luck = '';
   if( num==1 ) luck = '大吉';
-  else if( num==2 ) luck = '中吉';
+  else if( num==2 ) luck = '吉';
+  else if (num == 3) luck = '中吉';
+  else if (num == 4) luck = '小吉';
+  else if (num == 5) luck = '末吉';
+  else if (num == 6) luck = '凶';
+
 
   res.render( 'omikuji2', {result:luck} );
 });
 
+app.get("/omikuji3", (req, res) => {
+  const num = Math.floor(Math.random() * 6 + 1);
+  let luck = '';
+  let comment = '';
+  if (num == 1) {
+    luck = '大吉';
+    comment = '絶好調です！';
+  }
+  else if (num == 2) {
+    luck = '吉';
+    comment = '単位取得';
+  }
+  else if (num == 3) {
+    luck = '中吉';
+    comment = '普通...ってコト!?';
+  }
+  else if (num == 4) {
+    luck = '小吉';
+    comment = 'あんたつまんねえなァ!';
+  }
+  else if (num == 5) {
+    luck = '末吉';
+    comment = 'くそっ!お前はいつもそうだ';
+  }
+  else if (num == 6) {
+    luck = '凶';
+    comment = '誰もお前を愛さない';
+  }
+
+  res.render('omikuji3', { result: luck, comment: comment });
+});
+
+app.get("/janken", (req, res) => {
+  let hand = req.query.hand;
+  let win = Number(req.query.win);
+  let total = Number(req.query.total);
+  console.log({ hand, win, total });
+  const num = Math.floor(Math.random() * 3 + 1);
+  let cpu = '';
+  let judgement = '';
+  if (num == 1) cpu = 'グー';
+  else if (num == 2) cpu = 'チョキ';
+  else if (num == 3) cpu = 'パー';
+  // ここに勝敗の判定を入れる
+  // 以下の数行は人間の勝ちの場合の処理なので，
+  // 判定に沿ってあいこと負けの処理を追加する
+  if(num == 1)
+  {
+    judgement = '勝ち';
+    win += 1;
+    total += 1;
+  }
+  else if(num == 2)
+  {
+    judgement = '勝ち';
+    win += 1;
+    total += 1;
+  }
+  else if (num == 3)
+  {
+    judgement = '勝ち';
+    win += 1;
+    total += 1;
+  }
+
+  const display = {
+    your: hand,
+    cpu: cpu,
+    judgement: judgement,
+    win: win,
+    total: total
+  }
+  
+  res.render('janken_01', display);
+});
+
+/*
 app.get("/janken", (req, res) => {
   let hand = req.query.hand;
   let win = Number( req.query.win );
@@ -61,6 +151,32 @@ app.get("/janken", (req, res) => {
     total: total
   }
   res.render( 'janken', display );
+});
+*/
+
+let station = [
+  { id: 1, code: "JE01", name: "東京駅" },
+  { id: 2, code: "JE07", name: "舞浜駅" },
+  { id: 3, code: "JE12", name: "新習志野駅" },
+  { id: 4, code: "JE13", name: "幕張豊砂駅" },
+  { id: 5, code: "JE14", name: "海浜幕張駅" },
+  { id: 6, code: "JE05", name: "新浦安駅" },
+];
+
+app.get("/keiyo", (req, res) => {
+  // 本来ならここにDBとのやり取りが入る
+  res.render('db2', { data: station });
+});
+
+app.get("/keiyo_add", (req, res) => {
+  let id = req.query.id;
+  let code = req.query.code;
+  let name = req.query.name;
+  let newdata = { id: id, code: code, name: name };
+  station.push(newdata);
+  res.redirect('/public/keiyo_add.html');
+  //res.render('db2', { data: station });
+
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
--- wpro06.md
+++ wpro06.md
@@ -25,3 +25,32 @@ hand | text | ユーザの手 | グー/チョキ/パー
 win | hidden | 勝利数 | これまでの勝利数
 total | hidden | 対戦数 | これまでの対戦数
 
+#### やってみよう6
+```mermaid
+
+stateDiagram-v2
+[*] --> entry:ランディングページ
+entry:/public/index.html
+entry --> /omikuji1:omikuji1
+entry --> /omikuji2:omikuji2
+entry --> /omikuji3:omikuji3
+
+```
+
+### やってみよう7
+#### データ登録画面にリダイレクトする
+```mermaid
+stateDiagram-v2
+    [*] --> /public/keiyo_add.html
+    /public/keiyo_add.html --> /keiyo_add
+    /keiyo_add --> /public/keiyo_add.html:リダイレクト
+```
+
+#### 一覧表示する
+```mermaid
+stateDiagram-v2
+    [*] --> /public/keiyo_add.html
+    /public/keiyo_add.html --> /keiyo_add
+    /keiyo_add --> /view/keiyo.ejsによる一覧表示
+```
+
